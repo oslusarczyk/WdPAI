57c33378975cc6aebd7445a4d5bd2685f2c26e43
Oskar <oskar.slusarczyk77@gmail.com>
2024-05-10 18:40:14 +0200

    added navigation

--- public/views/main.php
+++ public/views/main.php
@@ -5,15 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/css/basic_styling.css">
-    <link rel="stylesheet" href="public/css/login.css">
-
+    <link rel="stylesheet" href="public/css/main.css"> 
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>STRONA GŁÓWNA</title>
 </head>
 <body>
+    <nav>
     <div class="header_logo">
         <img src="public\img\smartcar_logo.png" alt="SmartCar logo">
     </div>
-    <h2>strona główna</h2>
+    <div class="navParts">
+    <div class="mainPart">
+    <a href=""><i class='bx bx-home'></i><p>główna</p></a>
+    <a href=""><i class='bx bx-search'></i><p>wyszukaj</p></a>
+    <a href=""><i class='bx bx-history'></i><p>historia</p></a>
+    </div>
+    <div class="secondPart">
+        <a href=""><i class='bx bx-log-out'></i><p>wyloguj</p></a>
+    </div>
+    </div>
+    
+    </nav>
     </div>
 </body>
 </html>
