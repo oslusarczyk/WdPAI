f121a8e27d4e9a7f84d4f0d085da740917367d12
Oskar <oskar.slusarczyk77@gmail.com>
2024-05-11 19:43:44 +0200

    nav refactor

--- public/views/shared/nav.php
+++ public/views/shared/nav.php
@@ -1,24 +1,29 @@
 <nav>
-        <div class="header_logo">
-            <img src="public\img\smartcar_logo.png" alt="SmartCar logo">
-        </div>
-        <div class="navParts">
-            <div class="mainPart">
-                <a href=""><i class='bx bx-home'></i>
-                    <p>główna</p>
-                </a>
-                <a href=""><i class='bx bx-search'></i>
-                    <p>wyszukaj</p>
-                </a>
-                <a href=""><i class='bx bx-history'></i>
-                    <p>historia</p>
-                </a>
-            </div>
-            <div class="secondPart">
-                <a href="/logout"><i class='bx bx-log-out'></i>
-                    <p>wyloguj</p>
-                </a>
-            </div>
-        </div>
-        </div>
-    </nav>
+    <div class="header_logo">
+        <img src="public\img\smartcar_logo.png" alt="SmartCar logo">
+    </div>
+    <div class="navParts">
+        <a href=""><i class='bx bx-home'></i>
+            <p>główna</p>
+        </a>
+        <a href=""><i class='bx bx-search'></i>
+            <p>wyszukaj</p>
+        </a>
+        <a href=""><i class='bx bx-history'></i>
+            <p>historia</p>
+        </a>
+        <a class="end" href="/logout"><i class='bx bx-log-out'></i>
+            <p>wyloguj</p>
+        </a>
+        <a class="end" href="/logout"><i class='bx bx-log-out'></i>
+            <p>wyloguj</p>
+        </a>
+        <a class="end" href="/logout"><i class='bx bx-log-out'></i>
+            <p>wyloguj</p>
+        </a>
+        <a class="end" href="/logout"><i class='bx bx-log-out'></i>
+            <p>wyloguj</p>
+        </a>
+    </div>
+    </div>
+</nav>
